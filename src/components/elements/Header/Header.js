2e7f69ee5dff748d759b7f7cf375cb0398662e79
Zvonimir Dragun <zed.dragun@gmail.com>
2019-11-06 22:14:40 +0700

    first update

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
-        <div className="rmdb-header">
-            <div className="rmdb-header-content">
-                <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo" />
-                <img className="rmdb-tmdb-logo" src="./images/tmdb_logo.png" alt="tmdb-logo" />
-            </div>
+      <div className="rmdb-header">
+        <div className="rmdb-header-content">
+          <Link to="/">
+            <img
+              className="rmdb-logo"
+              src="./images/reactMovie_logo.png"
+              alt="rmdb-logo"
+            />
+          </Link>
+          <a href="https://www.themoviedb.org/?language=en-US">
+            <img
+              className="rmdb-tmdb-logo"
+              src="./images/tmdb_logo.png"
+              alt="tmdb-logo"
+            />
+          </a>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Header;
